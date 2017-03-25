4c40e6b6eb00dcd8618951d7f6af35533844b7b2
achujelas123 <achujel3@gmail.com>
2017-03-25 23:48:04 +0200

    adding some url tests

--- src/com/servletdemo/samples/DemoServlet.java
+++ src/com/servletdemo/samples/DemoServlet.java
@@ -8,9 +8,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "DemoServlet", urlPatterns = {"/a/b/c", "/servlets/hey"})
+@WebServlet(name = "DemoServlet", urlPatterns = {"/a/b/c", "/servlets/hey", "testingAnotherUrl"})
 public class DemoServlet extends HttpServlet {
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("GET method initialized");
         response.setContentType("text/html");
